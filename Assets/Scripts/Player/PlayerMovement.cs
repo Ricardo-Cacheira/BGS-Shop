c93915a24ff069a44368effcc9a7d6739a57d80d
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2023-04-18 16:13:36 +0100

    Update Movement script to use animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,19 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator animator;
 
     [SerializeField] private float speed = 7;
+    [SerializeField] private Transform waist;
     private Vector2 direction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         direction = input.normalized;
+
+        if(direction.magnitude > 0)
+            animator.Play("Walking");
+        else
+            animator.Play("Idle");
+
+        if(input.x != 0)
+        {
+            waist.localScale = new Vector3(input.x < 0 ? -1 : 1, 1,1);
+        }
+        
     }
 
     private void FixedUpdate()
